08417fc0f088ddfce284ffae2133e0cd7c1b926a
JohnBay <moneymashi@gmail.com>
2017-08-21 19:33:19 +0900

    ++ crawl + DF + MariaDB Done.

--- crawling/finalCrawling.py
+++ crawling/finalCrawling.py
@@ -4,24 +4,38 @@ Created on 2017. 8. 9.
 @author: acorn
 '''
 """
-http://creativeworks.tistory.com/entry/PYTHON-3-Tutorials-24-%EC%9B%B9-%ED%81%AC%EB%A1%A4%EB%9F%AClike-Google-%EB%A7%8C%EB%93%A4%EA%B8%B0-1-How-to-build-a-web-crawler
 
 http://market.cetizen.com/market.php?q=market&auc_sale=1&escrow_motion=3&sc=1&qs=&auc_wireless=&auc_uid=&stype=&akeyword=&just_one=&just_one_name=&just_one_pcat=&view_type=&m%5B1%5D=1&auc_price1=&auc_price2=&keyword_p=&pno=&pw=&p=1
 
+@@@@@ 참고
+@ 웹 크롤링
+http://creativeworks.tistory.com/entry/PYTHON-3-Tutorials-24-%EC%9B%B9-%ED%81%AC%EB%A1%A4%EB%9F%AClike-Google-%EB%A7%8C%EB%93%A4%EA%B8%B0-1-How-to-build-a-web-crawler
+@ DataFrame -> mariaDB 방법.
+http://creativeworks.tistory.com/entry/how-to-insert-dataframe-data-into-mysql-database-using-pymysqlpure-python3-library
+@ DB배우기.
+http://blog.naver.com/PostView.nhn?blogId=hmkuak&logNo=220583392375
 
 ######
-거의완성본@@ 
 Aug21, 2017
-12:48 workDone upto debug #2
-
+19:30 in work
 JB
 
- ## TODO:: 
-1. soldTitles의 경우, 한 페이지에 같은 제목 복수개중 첫번째 값만 가져온다. 모두 찾기 방법 모색.
+Worked
+------- Aug 21, 2017
 2. 마지막4개. 분석하는 변수중 하나로 쓸만할듯.        
->>3. df form.
+3. df form.
 4. put in mariaDB.
-   
+
+
+ ## TODO:: 
+-------- Aug 22, 2017
+00. 가독성증가용 :: 함수/ 클래스화. 제발 ㅜㅜ
+1. auc_no따오고 판매자 정보(가입일자 : 2016.05, 6개월간 판매 732 건   (거래완료 598 건   판매취소 1 건   구매취소 72 건   반품 61 건)), 판매자평가, 
+2. last4 options>> 설명나누기.
+3. 슬슬 데이터 -> matplot사용해서 그래프그리기.
+4. word clouding
+
+??. soldTitles의 경우, 한 페이지에 같은 제목 복수개중 첫번째 값만 가져온다. 모두 찾기 방법 모색.
 
 
 """
@@ -29,6 +43,9 @@ import requests
 import pandas as pd
 from pandas import DataFrame, Series
 from bs4 import BeautifulSoup as BS
+import mysql.connector
+from sqlalchemy import create_engine
+import pymysql, sys
 
 def spider(startPage, endPage):
     page = startPage
@@ -96,13 +113,6 @@ def spider(startPage, endPage):
         for check in lenList:
             if len(check) != 20:
                 print('length not 20. :: len:', len(check),'   list:: ', check )
-        # show result
-#         print('models :: ',models)    
-#         print('titles :: ',titles)    
-#         print('prices :: ',prices)    
-#         print('deliverFees :: ',deliverFees)    
-#         print('sellerNames :: ',sellerNames)    
-#         print('sellTimes :: ',sellTimes)    
         
         # 팔린물건 존재할경우 print.   
         if len(soldTitles) != 0:
@@ -126,16 +136,7 @@ def spider(startPage, endPage):
         print(df.describe())
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        dfToMaria(df)
         
         # 리스트 초기화
         soldTitles = []
@@ -148,12 +149,34 @@ def spider(startPage, endPage):
         sellTimes = []
         options = []
         
-        
-        
         page+=1
 
 
+def dfToMaria(df):
+    ###Anaconda prompt:: pip install mysql-connector
+    ###Anaconda prompt:: pip install mysql-connector-python-rf
+    
+    try:
+        con = pymysql.connect(host = 'localhost', port = 3306, 
+                              user = 'root', passwd = '11111', 
+                              db = 'crawl', charset = 'utf8')
+        engine = create_engine('mysql+mysqlconnector://root:11111@localhost:3306/crawl', echo=False)
+        df.to_sql(name='sample_table4', con=engine, if_exists = 'append', index=False)
+        
+        con.commit()
+        print('-'*30, ' insert df in Maria :: Well done')
+    
+    except:
+        print('-'*30, ' insert df in Maria :: went Wrong ::\n', sys.exc_info())
+        
+        con.rollback()
+    finally:
+        print('='*30, ' insert df in Maria :: finally Done')
+        con.close()
 
-spider(18, 25) ## page start, end.
+
+
+
+spider(3, 5) ## page start, end.
 
 
