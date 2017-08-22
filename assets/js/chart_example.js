2c9c8a336592c6f0630f557cf9ee62f916cf038a
Park, Yunha <hadbsgk@gmail.com>
2017-08-22 15:00:11 +0900

    add chartjs plugin & improve graph.html

--- assets/js/chart_example.js
+++ assets/js/chart_example.js
@@ -2,13 +2,12 @@ var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["8/10", "8/11", "8/12", "8/13", "8/14", "8/15"],
         datasets: [{
-            label: '# of Votes',
+            label: 'price of iPhone7',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
-                
             ],
             borderColor: [
                 'rgba(255,99,132,1)',
@@ -17,10 +16,49 @@ var myChart = new Chart(ctx, {
         }]
     },
     options: {
+        responsive: true,
+        title:{
+            display:true,
+            text:'iPhone7'
+        },
         scales: {
+            xAxes: [{
+                display: true,
+            }],
             yAxes: [{
-                ticks: {
-                    beginAtZero:true
+                display: true,
+                // type: 'logarithmic',
+            }]
+        },
+        annotation: {
+            events: ['click'],
+            annotations: [{
+                drawTime: 'afterDraw',
+                id: 'hline',
+                type: 'line',
+                mode: 'horizontal',
+                scaleID: 'y-axis-0',
+                value: 7,
+                borderColor: 'red',
+                borderWidth: 2,
+                label: {
+                    backgroundColor: "red",
+                    content: "Mean",
+                    enabled: true
+                }
+            }, {
+                drawTime: 'afterDraw',
+                id: 'vline',
+                type: 'line',
+                mode: 'vertical',
+                scaleID: 'x-axis-0',
+                value: '8/12',
+                borderColor: 'orange',
+                borderWidth: 2,
+                label: {
+                    backgroundColor: "red",
+                    content: "Release Day",
+                    enabled: true
                 }
             }]
         }
